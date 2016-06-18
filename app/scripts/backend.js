cb8dfa264a9f710c706d96d221a38bf5cd8b4137
Christopher Hall <Biomech.ArkahnX@gmail.com>
2016-06-18 02:44:15 -0700

    Added backup if on the dev version.

--- app/scripts/backend.js
+++ app/scripts/backend.js
@@ -3,6 +3,38 @@ if (manifest.key) {
 	window.console.time = function() {};
 	window.console.timeEnd = function() {};
 }
+if (!manifest.key) {
+	backupHistory();
+}
+
+function backupHistory() {
+	chrome.permissions.contains({
+		permissions: ['downloads']
+	}, function(result) {
+		if (result) {
+			_backup();
+		} else {
+			chrome.permissions.request({
+				permissions: ['downloads']
+			}, function(granted) {
+				if (granted) {
+					_backup();
+				}
+			});
+		}
+	});
+	setTimeout(backupHistory,1000*60*30);
+}
+
+function _backup() {
+	chrome.storage.local.get(null, function(data) {
+		var url = 'data:application/json;base64,' + btoa(JSON.stringify(data.itemChanges));
+		chrome.downloads.download({
+			url: url,
+			filename: `itemChanges-${moment().format("YYYY-MM-DD_hh-mm-A")}.json`
+		});
+	});
+}
 
 function appClicked() {
 	var appUrl = chrome.extension.getURL('index.html');
