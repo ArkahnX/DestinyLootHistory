570fe4194b9c287047f71f35a609ae2a80f6cd24
Christopher Hall <Biomech.ArkahnX@gmail.com>
2016-07-05 12:43:36 -0700

    Remove GAQ code

--- app/scripts/pages/history.js
+++ app/scripts/pages/history.js
@@ -327,12 +327,5 @@ function buildCompactItem(itemData, bucketHash) {
 }
 // var bungie = new Bungie();
 initItems(function() {
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-77020265-2', 'auto');
-ga('set', 'checkProtocolTask', null);
-ga('send', 'pageview');
 });
--- app/scripts/pages/options.js
+++ app/scripts/pages/options.js
@@ -96,11 +96,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	dropZone.addEventListener('dragover', handleDragOver, false);
 	dropZone.addEventListener('drop', handleFileSelect, false);
 });
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-77020265-2', 'auto');
-ga('set', 'checkProtocolTask', null);
-ga('send', 'pageview');
