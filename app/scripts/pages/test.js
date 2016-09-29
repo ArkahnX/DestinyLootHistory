ffa2df8c6b4c473ae3cd00b83559997b0eb9bbc6
Christopher Hall <biomech.arkahnx@gmail.com>
2016-09-29 13:06:03 -0700

    v1.4.5 UI changes to all pages (#76)
    Autolock light level targets all rarity
    
    Three of coins reminder will look on other characters now
    
    New manifest version

--- app/scripts/pages/test.js
+++ app/scripts/pages/test.js
@@ -82,6 +82,7 @@ function checkInventory() {
 		// sequence(characterIdList, factionNetworkTask, factionResultTask).then(function() {
 		var characterHistory = document.getElementById("history");
 		var inventoryData = [];
+		var sortedInventoryData = {};
 		for (var inventory of data.inventories) {
 			if (inventoryData.length === 0) {
 				Array.prototype.push.apply(inventoryData, inventory.inventory);
@@ -107,34 +108,52 @@ function checkInventory() {
 				Array.prototype.push.apply(inventoryData, arrayToMerge);
 			}
 		}
-		inventoryData.sort(function(a, b) {
-			if (!isNaN(parseInt(a.stackSize))) {
-				return parseInt(b.stackSize) - parseInt(a.stackSize);
-			} else if (a.primaryStat && b.primaryStat && !isNaN(parseInt(a.primaryStat.value))) {
-				return parseInt(b.stackSize) - parseInt(a.stackSize);
-			} else {
-				return a.itemInstanceId - b.itemInstanceId;
+		for (let item of inventoryData) {
+			let itemDefinition = getItemDefinition(item.itemHash);
+			let bucketName = DestinyInventoryBucketDefinition[itemDefinition.bucketTypeHash] && DestinyInventoryBucketDefinition[itemDefinition.bucketTypeHash].bucketName || itemDefinition.bucketTypeHash;
+			if (!sortedInventoryData[bucketName]) {
+				sortedInventoryData[bucketName] = [];
 			}
-		});
-		console.log(inventoryData)
-		var containingDiv = null;
-		for (var item of inventoryData) {
-			var itemDefinition = getItemDefinition(item.itemHash);
-			var bucketName = DestinyInventoryBucketDefinition[itemDefinition.bucketTypeHash] && DestinyInventoryBucketDefinition[itemDefinition.bucketTypeHash].bucketName || itemDefinition.bucketTypeHash;
-			if (document.getElementById(bucketName) === null) {
-				var div = document.createElement("div");
-				div.classList.add("sub-section");
-				var description = document.createElement("h1");
-				description.textContent = bucketName;
-				div.appendChild(description);
-				characterHistory.appendChild(div);
-				var nodeList = document.createElement("div");
-				nodeList.classList.add("sub-section");
-				nodeList.id = bucketName;
-				characterHistory.appendChild(nodeList);
+			sortedInventoryData[bucketName].push(item);
+		}
+		for (let bucket of sortedInventoryData) {
+			bucket.sort(function(a, b) {
+				if(a.primaryStat && !b.primaryStat) {
+					return 0 - parseInt(a.primaryStat.value);
+				} else if(b.primaryStat && !a.primaryStat) {
+					return parseInt(b.primaryStat.value) - 0;
+				} else if (a.primaryStat && b.primaryStat && !isNaN(parseInt(a.primaryStat.value))) {
+					return parseInt(b.primaryStat.value) - parseInt(a.primaryStat.value);
+				} else if(!a.primaryStat && !b.primaryStat && a.stackSize === b.stackSize) {
+					return b.itemInstanceId - a.itemInstanceId;
+				} else if (!isNaN(parseInt(a.stackSize))) {
+					return parseInt(b.stackSize) - parseInt(a.stackSize);
+				} else {
+					return b.itemInstanceId - a.itemInstanceId;
+				}
+			});
+		}
+		console.log(sortedInventoryData);
+		let containingDiv = null;
+		for (let bucket of sortedInventoryData) {
+			for (let item of bucket) {
+				var itemDefinition = getItemDefinition(item.itemHash);
+				var bucketName = DestinyInventoryBucketDefinition[itemDefinition.bucketTypeHash] && DestinyInventoryBucketDefinition[itemDefinition.bucketTypeHash].bucketName || itemDefinition.bucketTypeHash;
+				if (document.getElementById(bucketName) === null) {
+					var div = document.createElement("div");
+					div.classList.add("sub-section");
+					var description = document.createElement("h1");
+					description.textContent = bucketName;
+					div.appendChild(description);
+					characterHistory.appendChild(div);
+					var nodeList = document.createElement("div");
+					nodeList.classList.add("sub-section");
+					nodeList.id = bucketName;
+					characterHistory.appendChild(nodeList);
+				}
+				containingDiv = document.getElementById(bucketName);
+				containingDiv.appendChild(makeItem(item, "vault"));
 			}
-			containingDiv = document.getElementById(bucketName);
-			containingDiv.appendChild(makeItem(item, "vault"));
 		}
 		console.timeEnd("Bungie Inventory");
 	});
