103c3c6592e5e62717968e4b962a7de2bf6d6b7c
Christopher Hall <Biomech.ArkahnX@gmail.com>
2016-11-16 22:22:57 -0800

    added status page
    fixed bug with options page

---
+++ app/scripts/pages/status.js
@@ -0,0 +1,182 @@
+tracker.sendAppView('StatusScreen');
+var cookieResults = {
+	bungled: "",
+	bungleatk: ""
+};
+var systemDetails = {
+	xbo: {
+		id: "",
+		type: 1,
+		membership: "",
+		characters: []
+	},
+	ps4: {
+		id: "",
+		type: 2,
+		membership: "",
+		characters: []
+	}
+};
+
+function handleResultDisplay(result, messages, className, icon, resolve) {
+	if (result === 0) {
+		messages.unshift("Success");
+		document.getElementById(className).className = "success";
+		icon = "check";
+	}
+	document.getElementById(className).innerHTML = `<i class="fa fa-${icon}" aria-hidden="true"></i><pre>${result}</pre> - <pre>${messages.join(", ")}</pre>`;
+	resolve();
+}
+
+function request(opts) {
+	let r = new XMLHttpRequest();
+	r.open(opts.method, "https://www.bungie.net/Platform" + opts.route, true);
+	r.setRequestHeader('X-API-Key', API_KEY);
+	r.onload = function() {
+		if (this.status >= 200 && this.status < 400) {
+			var response = JSON.parse(this.response);
+			opts.complete(response);
+		}
+	};
+	r.onerror = function(error) {
+		console.error(error);
+		opts.incomplete();
+	};
+	r.withCredentials = true;
+	r.setRequestHeader('x-csrf', cookieResults.bungled);
+	opts.csrf = cookieResults.bungled;
+	opts.atk = cookieResults.bungleatk.length;
+	r.send(JSON.stringify(opts.payload));
+}
+
+function bungieLogin() {
+	var result = 0;
+	var messages = [];
+	var icon = "exclamation";
+	return new Promise(function(resolve) {
+		request({
+			route: '/User/GetBungieNetUser/',
+			method: 'GET',
+			incomplete: function() {
+				console.error("Empty incomplete");
+			},
+			complete: function(res) {
+				console.log(res);
+				if (res.ErrorCode === 1) {
+					var data = res.Response;
+					if (data.gamerTag && data.publicCredentialTypes.indexOf(1) > -1) {
+						systemDetails.xbo.id = data.gamerTag;
+						messages.push("XBL: " + data.gamerTag);
+					}
+					if (data.psnId && data.publicCredentialTypes.indexOf(2) > -1) {
+						systemDetails.ps4.id = data.psnId;
+						messages.push("PSN: " + data.psnId);
+					}
+					if (!systemDetails.xbo.id && !systemDetails.ps4.id) {
+						result = 1;
+						messages.push("Unable to find linked PSN or XBL account names.");
+					}
+				} else {
+					result = res.ErrorCode;
+					messages.push(res.ErrorStatus);
+				}
+				console.log("bungielogin", result, messages, systemDetails);
+				handleResultDisplay(result, messages, "bungielogin", icon, resolve);
+			}
+		});
+	});
+}
+
+function bungieConsoleData(systemDetail, outputId) {
+	var result = 0;
+	var messages = [];
+	var icon = "exclamation";
+	return new Promise(function(resolve) {
+		request({
+			route: '/Destiny/' + systemDetail.type + '/Stats/GetMembershipIdByDisplayName/' + systemDetail.id + '/',
+			method: 'GET',
+			incomplete: function() {
+				console.error("Empty incomplete");
+			},
+			complete: function(response) {
+				console.log("membership", response);
+				if (response.ErrorCode === 1) {
+					systemDetail.membership = response.Response;
+					request({
+						route: '/Destiny/' + systemDetail.type + '/Account/' + response.Response + '/Summary/',
+						method: 'GET',
+						incomplete: function() {
+							console.error("Empty incomplete");
+						},
+						complete: function(account) {
+							console.log("account", account);
+							if (account.ErrorCode === 1) {
+								for (var character of account.Response.data.characters) {
+									systemDetail.characters.push(character.characterBase.characterId);
+								}
+								messages.push("Characters: " + systemDetail.characters.length);
+							} else {
+								result = response.ErrorCode;
+								messages.push(response.ErrorStatus);
+							}
+							handleResultDisplay(result, messages, outputId, icon, resolve);
+						}
+					});
+				} else {
+					result = response.ErrorCode;
+					messages.push(response.ErrorStatus);
+					handleResultDisplay(result, messages, outputId, icon, resolve);
+				}
+
+
+			}
+		});
+	});
+}
+
+
+
+function bungieCookieStep() {
+	return new Promise(function(resolve) {
+		var result = 0;
+		var messages = [];
+		var icon = "exclamation";
+		chrome.cookies.getAll({
+			domain: "www.bungie.net"
+		}, function(cookies) {
+			if (chrome.runtime.lastError) {
+				tracker.sendEvent('unable to read cookies', JSON.stringify(chrome.runtime.lastError), `version ${localStorage.version}, systems ${localStorage.systems}`);
+				result = 1;
+				messages.push(chrome.runtime.lastError);
+				console.error(chrome.runtime.lastError);
+			}
+			if (Array.isArray(cookies)) {
+				for (var cookie of cookies) {
+					if (cookie.name && cookie.value) {
+						if (cookie.name === "bungled" || cookie.name === "bungleatk") {
+							cookieResults[cookie.name] = cookie.value;
+						}
+					}
+				}
+			}
+			if (cookieResults.bungled.length && !cookieResults.bungleatk.length) {
+				result = 2;
+				messages.push("Missing bungleatk cookie");
+			}
+			if (!cookieResults.bungled.length && cookieResults.bungleatk.length) {
+				result = 3;
+				messages.push("Missing bungled cookie");
+			}
+			console.log("Cookies", result, messages, messages.length === 1 ? messages[0] : messages.join(", "), cookieResults);
+			handleResultDisplay(result, messages, "cookies", icon, resolve);
+		});
+	});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	bungieCookieStep().then(bungieLogin).then(function() {
+		return bungieConsoleData(systemDetails.xbo, "bungiexbone");
+	}).then(function() {
+		return bungieConsoleData(systemDetails.ps4, "bungieps4");
+	});
+});
