a533e311e62e7dd4852bef9438cdfcd1ae91580d
Christopher Hall <Biomech.ArkahnX@gmail.com>
2017-02-05 13:17:37 -0800

    New manifest version

--- app/scripts/pages/auth.js
+++ app/scripts/pages/auth.js
@@ -1,12 +1,12 @@
 document.getElementById("bungieLoginButton").setAttribute("HREF", AUTH_URL);
-tracker.sendEvent('Auth', 'Open', "True");
+tracker.sendEvent('Auth', 'Open', localStorage.systems);
 if (location.search.length && location.search.split("=")[1]) {
 	document.getElementById("bungieLoginButton").setAttribute("disabled", true);
 	document.getElementById("bungieLoginButton").value = "Processing Data...";
 	chrome.runtime.sendMessage({
 		authCode: location.search.split("=")[1]
 	});
-	tracker.sendEvent('Auth', 'Login', "True");
+	tracker.sendEvent('Auth', 'Login', localStorage.systems);
 } else if (location.hash === "#login") {
 	chrome.runtime.getBackgroundPage(function (backgroundPage) {
 		backgroundPage.stopTimer("extensionIcon");
@@ -18,7 +18,7 @@ if (location.search.length && location.search.split("=")[1]) {
 		localStorage.threeOfCoinsProgress = "{}";
 	});
 } else if (location.hash === "#logout") {
-	tracker.sendEvent('Auth', 'Logout', "Page");
+	tracker.sendEvent('Auth', 'Logout', localStorage.systems);
 	// location.href = "auth.html#login";
 	chrome.runtime.getBackgroundPage(function (backgroundPage) {
 		backgroundPage.stopTimer("extensionIcon");
@@ -35,7 +35,7 @@ if (location.search.length && location.search.split("=")[1]) {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.loggedIn) {
-		tracker.sendEvent('Auth', 'Redirect', "True");
+		tracker.sendEvent('Auth', 'Redirect', localStorage.systems);
 		location.href = "index.html";
 	}
 });
